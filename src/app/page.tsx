a577b1d82f0e5ee4e4174486aef6f2b5164f39b1
Ayush tomar <ayushtomar388@gmail.com>
2024-11-02 21:57:55 +0530

    Hero Area added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
+import HeroArea from "@/sections/HeroArea";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <HeroArea/>
       <Footer/>
     </>
   );
